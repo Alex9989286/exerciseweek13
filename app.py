93e4962102bed2def1d04dbaf754d290adc78b81
Alex <jianshengalexng17317@gmail.com>
2026-07-03 20:41:11 +0800

    app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st, requests
 
-N8N = "https://exerciseweek13-1-tonp.onrender.com"
+N8N = "https://alexngjansheng.app.n8n.cloud/webhook-tests"
 
 
 st.title("Smart Customer Retention Assistant")
